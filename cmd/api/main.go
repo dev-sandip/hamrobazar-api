5d7171d0958917de9d57a3e319536e24e2841128
Sandip Sapkota <sandipsapkota9844@gmail.com>
2026-09-20 09:08:40 +0545

    config setup

---
+++ cmd/api/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
+
+	"github.com/dev-sandip/hamrobazar-api/cmd/internal/config"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status": "ok"}`))
+	})
+
+	srv := &http.Server{
+		Addr:         ":" + cfg.Port,
+		Handler:      mux,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 30,
+		IdleTimeout:  time.Second * 60,
+	}
+
+	log.Printf("Server is running on port %s", cfg.Port)
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("Server Failed : %v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dev-sandip/hamrobazar-api
+
+go 1.26.8
+
+require github.com/joho/godotenv v1.5.1 // indirect
